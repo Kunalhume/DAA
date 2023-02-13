109b44146c3049287bc70c0bf3b0d60289e18f47
Kunalhume <78203037+Kunalhume@users.noreply.github.com>
2023-02-14 01:08:40 +0530

    Kodec

---
+++ Pointers/explainPointer.cpp
@@ -0,0 +1,36 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int arr[]={2,5,7,8,1};
+  // cout<<arr<<"\n";
+  cout<<arr[2]<<"\n";
+
+  // Creating Pointer 'q'
+  int *q;
+  q=arr;
+
+  cout<<"Arr Pointer : "<<("%P",arr)<<"\n";
+  cout<<"New created pointer : "<<("%P",q)<<"\n";
+
+  cout<<"Arr Pointer value: "<<("%d",2[arr])<<"\n";
+  cout<<"New created pointer value : "<<("%d",q[2])<<"\n";
+
+
+  // Incrementing  Pointer 'arr'
+  // Incremented by int size(4) of 'arr'
+  // It's Pointing to next index of 'arr' or next element address
+  cout<<"Arr Pointer increment : "<<("%P",arr+1)<<"\n";
+
+  // Incrementing Address of 'arr'
+  // Incremented by total size of 'arr'
+  cout<<"&Arr Pointer increment : "<<("%P",&arr+1)<<"\n";
+  
+  
+  cout<<"&Arr[index] Pointer increment : "<<("%P",&arr[0]+1)<<"\n";
+
+
+  cout<<"Arr Pointer Address: "<<("%d",*arr+3)<<"\n";
+  
+  cout<<"Arr Pointer value: "<<("%d",*(arr+3))<<"\n";
+}
