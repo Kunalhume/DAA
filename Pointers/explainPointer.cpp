dfa17a9dd496d8ab6e9925e8f26843adac593bf7
Kunalhume <78203037+Kunalhume@users.noreply.github.com>
2023-02-14 01:24:47 +0530

    Kunal

--- Pointers/explainPointer.cpp
+++ Pointers/explainPointer.cpp
@@ -2,35 +2,53 @@
 using namespace std;
 
 int main(){
-  int arr[]={2,5,7,8,1};
-  // cout<<arr<<"\n";
-  cout<<arr[2]<<"\n";
+  // int arr[]={2,5,7,8,1};
+  // // cout<<arr<<"\n";
+  // cout<<arr[2]<<"\n";
 
-  // Creating Pointer 'q'
-  int *q;
-  q=arr;
+  // // Creating Pointer 'q'
+  // int *q;
+  // q=arr;
 
-  cout<<"Arr Pointer : "<<("%P",arr)<<"\n";
-  cout<<"New created pointer : "<<("%P",q)<<"\n";
+  // cout<<"Arr Pointer : "<<("%P",arr)<<"\n";
+  // cout<<"New created pointer : "<<("%P",q)<<"\n";
 
-  cout<<"Arr Pointer value: "<<("%d",2[arr])<<"\n";
-  cout<<"New created pointer value : "<<("%d",q[2])<<"\n";
+  // cout<<"Arr Pointer value: "<<("%d",2[arr])<<"\n";
+  // cout<<"New created pointer value : "<<("%d",q[2])<<"\n";
 
 
-  // Incrementing  Pointer 'arr'
-  // Incremented by int size(4) of 'arr'
-  // It's Pointing to next index of 'arr' or next element address
-  cout<<"Arr Pointer increment : "<<("%P",arr+1)<<"\n";
+  // // Incrementing  Pointer 'arr'
+  // // Incremented by int size(4) of 'arr'
+  // // It's Pointing to next index of 'arr' or next element address
+  // cout<<"Arr Pointer increment : "<<("%P",arr+1)<<"\n";
 
-  // Incrementing Address of 'arr'
-  // Incremented by total size of 'arr'
-  cout<<"&Arr Pointer increment : "<<("%P",&arr+1)<<"\n";
+  // // Incrementing Address of 'arr'
+  // // Incremented by total size of 'arr'
+  // cout<<"&Arr Pointer increment : "<<("%P",&arr+1)<<"\n";
   
   
-  cout<<"&Arr[index] Pointer increment : "<<("%P",&arr[0]+1)<<"\n";
+  // cout<<"&Arr[index] Pointer increment : "<<("%P",&arr[0]+1)<<"\n";
 
 
-  cout<<"Arr Pointer Address: "<<("%d",*arr+3)<<"\n";
+  // cout<<"Arr Pointer Address: "<<("%d",*arr+3)<<"\n";
   
-  cout<<"Arr Pointer value: "<<("%d",*(arr+3))<<"\n";
+  // cout<<"Arr Pointer value: "<<("%d",*(arr+3))<<"\n";
+
+  
+  // Taking Input array using Pointer 
+  int arr[5];
+  int *q = arr;
+  for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+  {
+    int x;
+    cin>>x;
+    i[q]=x;
+  }
+
+  for (int i = 0; i < *(&arr+1)-arr; i++)
+  {
+    cout<<*(q+i)<<" ";
+  }
+  
+  
 }
